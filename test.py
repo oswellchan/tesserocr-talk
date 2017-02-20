1f978494fe686fd50783f6f4dac63c7eddd2eef3
Oswell Chan <oswellchan@gmail.com>
2017-02-20 10:55:47 +0800

    Clean up unused files

--- test.py
+++
@@ -1,8 +0,0 @@
-import tesserocr
-from PIL import Image
-
-print(tesserocr.tesseract_version())  # print tesseract-ocr version
-print(tesserocr.get_languages())  # prints tessdata path and list of available languages
-
-image = Image.open('output0.jpg')
-print(tesserocr.image_to_text(image))  # print ocr text from image #
